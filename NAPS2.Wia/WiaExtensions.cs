ad6b039277beb61abb8674d2f6c9e6bb1510bc3a
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-04-07 17:34:18 -0700

    Fix setting range value

--- NAPS2.Wia/WiaExtensions.cs
+++ NAPS2.Wia/WiaExtensions.cs
@@ -76,6 +76,7 @@ namespace NAPS2.Wia
                     {
                         value -= (value - prop.Attributes.Min) % prop.Attributes.Step;
                     }
+                    prop.Value = value;
                 }
                 else
                 {
